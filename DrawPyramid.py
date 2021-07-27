821dd7a9c80efc05cbe79e45b418dd23644ad344
mitchellhu <mitchellhmy@gmail.com>
2021-07-27 14:47:21 +0800

    modify DrawPyramid.py

--- DrawPyramid.py
+++ DrawPyramid.py
@@ -1,8 +1,7 @@
-starlist = [1,3,5,7,9]
-maxwidth = max(starlist)
 blankSpace = ' '
 startSpace = '*'
 tempcount = 0
+maxwidth = 0
 
 def printLine(level):
     startline = ''
@@ -11,8 +10,25 @@ def printLine(level):
     starline = startSpace * level
     tempcount = int((maxwidth - level) / 2)
     spaceLeft = blankSpace * tempcount
-    spaceRight = spaceLeft
-    print(spaceLeft + starline + spaceRight)
+    print(spaceLeft + starline)
 
-for i in starlist:
-    printLine(i)
+def Is_evennumber(anumber):
+    if int(anumber) % 2 == 0:
+        return False
+    else:
+        return True
+
+
+while True:
+    usernumber = input('請輸入金字塔層數:')
+    if isinstance(usernumber, int):
+        print('請輸入整數數字！！！')
+        continue
+    if not Is_evennumber(usernumber):
+        print('請輸入單數整數數字！！！')
+        continue
+    print('\n')
+    maxwidth = int(usernumber);
+    for i in range(1,int(usernumber)+1,2):
+        printLine(i)
+    break
